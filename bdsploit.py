1b16b76d6f318b3a98b0249ef8f526ffe0d7088a
ebdecastro <elena.bdecastro@gmail.com>
2019-06-11 19:38:18 +0200

    [0.1.1] BDsploit functional final version 0.1.1. Some modifications added to correct bugs.

--- bdsploit.py
+++ bdsploit.py
@@ -3,6 +3,7 @@ import exploitation
 import postexploitation
 import c2
 import delete
+import time
 
 W  = '\033[0m'  # white (normal)
 R  = '\033[31m' # red
@@ -26,6 +27,8 @@ def bdsploit(url):
     print("***** To connect to the host use the following IP: {}{}{}".format(R, host, W))
     postexploitation.postexploit(host)
     c2.c2(host, "tools/done.shell", "/home/test/.ssh/id_rsa", "/usr/.bdsptc2.shell", "/usr/.bdsptc2.sh")
+    print("Sleeping 5 seconds to delete user and SSH key")
+    time.sleep(5)
     delete.delete(host, "/home/test/.ssh/id_rsa")
 
 
